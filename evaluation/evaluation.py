c0f17a0dbe7940ac026bdcece193d830020c2168
Michelle <jmichellechan@gmail.com>
2021-01-28 11:58:13 +0100

    update evaluation.py

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -16,21 +16,21 @@ from keras.applications.inception_v3 import preprocess_input
 
 from ndb import *
 
-# Read in folder containing generated images
-def read_file(img_dir_path):
-    folder_path = img_dir_path
-    imgs = []
-    for filename in glob.glob(os.path.join(folder_path, '*.png')):
-        img = cv2.imread(filename)
-        imgs.append(img)
-        print(filename)
-    return imgs  
+# # Read in folder containing generated images
+# def read_file(img_dir_path):
+#     folder_path = img_dir_path
+#     imgs = []
+#     for filename in glob.glob(os.path.join(folder_path, '*.png')):
+#         img = cv2.imread(filename)
+#         imgs.append(img)
+#         print(filename)
+#     return imgs
+#
+# def create_img_batch(imgs):
+#     return np.stack(tuple(imgs))
 
-def create_img_batch(imgs):
-    return np.stack(tuple(imgs))
 
-
-def calculate_inception_score(images, n_split=2, eps=1E-16):
+def calculate_inception_score(images, n_split=10, eps=1E-16):
         # load inception v3 model
         model = InceptionV3(include_top=False, weights='imagenet')
         
@@ -66,7 +66,7 @@ def calculate_inception_score(images, n_split=2, eps=1E-16):
 
 # calculate frechet inception distance
 def calculate_fid(images1, images2):
-    model = InceptionV3(include_top=False, pooling='avg', input_shape=(400,400,3))
+    model = InceptionV3(include_top=False, pooling='avg', input_shape=(96, 384, 3))
     images1 = preprocess_input(images1)    
     images2 = preprocess_input(images2)    
     # calculate activations
@@ -111,33 +111,79 @@ def calculate_bins(train_samples, test_samples, k):
     ndb.evaluate(test_samples, model_label='Test')
     # ndb.plot_results(models_to_plot=['Test']) gives errors
 
-def main():
-    img_folder = './generated_images/'
-    imgs = read_file(img_folder) 
-    images = create_img_batch(imgs)
 
+def main():
+    #     img_folder = './generated_images/'
+    #     imgs = read_file(img_folder)
+    #     images = create_img_batch(imgs)
+    objects = []
+    origin_path = Path("./")
+    for filename in origin_path.glob("*.pkl"):
+        with (open(filename, "rb")) as openfile:
+            while True:
+                try:
+                    objects.append(pickle.load(openfile))
+                except EOFError:
+                    break
+    with (open('./generated_images/generated_images.pkl', "rb")) as openfile:
+        test_images = pickle.load(openfile)
+
+    train_images = []
+    train_labels = []
+    width = 64
+    height = 32
+    EPOCHS = 10
+    num_examples_to_generate = 1
+
+    for inner_layer in objects:
+        label = inner_layer[0]
+        for i in range(1, len(inner_layer)):
+            for soundfile in inner_layer[i]:
+                num_images = len(soundfile[0]) // width
+                for j in range(num_images):
+                    index1 = j * width
+                    index2 = (j + 1) * width
+                    train_images.append(soundfile[0:width, index1:index2])
+                    train_labels.append(label)
+    train_images = np.array(train_images)
+    #train_labels = np.array(train_labels)
+    # parse data to floats:
+    train_images = train_images.reshape(train_images.shape[0], height, width, 1).astype('float32')
+    print('Done')
+    train_images_ = scale_images(train_images[:10000], (96, 384, 3))
+    # test_images = scale_images(images, (96, 384, 3))
+    # print(train_images_.shape)
+    #print(test_images.shape)
+    # train_images = np.stack((train_images,)*3, axis=-1)
+    # print(train_images.shape)
+    # train_samples = create_img_batch(train_images)
+    # print(train_samples.shape)
     # calculate inception score
-    is_avg, is_std = calculate_inception_score(images)
-    print('Inception score avg:', is_avg, "Inception score std:", is_std)
+    is_avg, is_std = calculate_inception_score(train_images_)
+    is_avg_t, is_std_t = calculate_inception_score(test_images)
+    print('TRAIN Inception score avg:', is_avg, "Inception score std:", is_std)
+    print('TEST Inception score avg:', is_avg_t, "Inception score std:", is_std_t)
     print("---------------")
-    # calculate fid
-    fid = calculate_fid(images, images)
+    fid = calculate_fid(train_images_, test_images)
     print('FID score: %.3f' % fid)
 
     batch = []
-    for i in imgs:
+    for i in train_images_:
         batch.append(i.ravel())
     images_ndb = np.array(batch)
 
-    num_bins = 2
-    n_train = round(images_ndb.shape[0] * 0.7)
+    for i in test_images:
+        batch.append(i.ravel())
+    test_images = np.array(batch)
 
-    rand_order = np.random.permutation(images_ndb.shape[0])
-    train_samples = images_ndb[rand_order[:n_train]]
-    test_samples = images_ndb[rand_order[n_train:]]
+    num_bins = 5
+    #     n_train = round(images_ndb.shape[0]*0.7)
 
-    # calculate NDB
-    calculate_bins(train_samples, test_samples, num_bins)
+    #     rand_order = np.random.permutation(images_ndb.shape[0])
+    #     train_samples = images_ndb[rand_order[:n_train]]
+    #     test_samples = images_ndb[rand_order[n_train:]]
+
+    calculate_bins(images_ndb, test_images, num_bins)
 
 if __name__ == '__main__':
     main()
